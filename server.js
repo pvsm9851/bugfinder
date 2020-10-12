6f2fa39cc1601054c1439ed2790d7c279ee9990c
Paulo Melo <paulo.melo@inmetrics.com.br>
2020-10-12 16:58:00 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
+const app = express()
+const uri = "mongodb+srv://tcc:bolinha123@cluster0.rpwsc.gcp.mongodb.net/bugfinderDev?retryWrites=true&w=majority"
+let db
+app.use(bodyParser.json())
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+
+    db = client.db('bugfinderDev')
+
+    app.listen(3000, function (){
+        console.log('server running on port 3000')
+    })
+})
+
+app.set('view engine', 'ejs')
+
+app.post('/sendData', (req, res)=>{
+    db.collection('dataAutomation').insert(req.body, (err, result)=> {
+        if (err) return console.log(err)
+
+        console.log(req.body)
+        res.send(req.body)
+    })
+})
+
+
+
+app.get('/', (req, res)=> {
+    res.render('index.ejs')
+})
